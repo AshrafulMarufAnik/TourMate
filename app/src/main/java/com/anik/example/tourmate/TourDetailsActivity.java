77422095767e4438984f3497db8290b0b2bde63c
DOLPHIN <ashraful.maruf.anik@gmail.com>
2019-08-09 23:27:49 +0600

    UI work

--- app/src/main/java/com/anik/example/tourmate/TourDetailsActivity.java
+++ app/src/main/java/com/anik/example/tourmate/TourDetailsActivity.java
@@ -2,7 +2,9 @@ package com.anik.example.tourmate;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class TourDetailsActivity extends AppCompatActivity {
 
@@ -16,5 +18,9 @@ public class TourDetailsActivity extends AppCompatActivity {
 
     private void init() {
 
+    }
+
+    public void goToMainActivity(View view) {
+        startActivity(new Intent(TourDetailsActivity.this,MainActivity.class));
     }
 }
